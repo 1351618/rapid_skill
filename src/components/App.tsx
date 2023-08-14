184583d3bc7ebd35b96be76029878a307cae4d6b
1351618 <1351618@gmail.com>
2023-08-14 19:07:08 +0300

    test_02

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        // <div className="App-blue">
+        <div className="App-red">
+            <p>123</p>
+        </div>
+    );
+}
+
+export default App;
